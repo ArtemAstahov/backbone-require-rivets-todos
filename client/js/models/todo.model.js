0b6d92bc7aa6f82fdfba9ddffc527d9217e88dcf
Steven Sojka <steelsojka@gmail.com>
2014-08-02 21:47:07 -0500

    remove some uneeded dom elements

--- client/js/models/todo.model.js
+++ client/js/models/todo.model.js
@@ -2,17 +2,14 @@ define([
     "backbone"
 ], function(Backbone) {
     return Backbone.Model.extend({
-        initialize: function() {
-            // We are using the timestamp as the UUID
-            // to sync across clients.
-            if (!this.get("timestamp")) {
-                this.set("timestamp", Date.now(), {silent: true});
-            }
-        },
-        defaults: {
-            timestamp: null,
-            value: "",
-            done: false
+        defaults: function() {
+            return {
+                // We are using the timestamp as the UUID
+                // to sync across clients.
+                timestamp: Date.now(),
+                value: "",
+                done: false
+            };
         }
     });
 });
